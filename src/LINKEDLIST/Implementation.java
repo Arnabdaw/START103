4603bb2dfca488421a04327e6ec6a12e40744589
Arnabdaw <arnabdaw1@gmail.com>
2024-10-21 05:01:30 +0530

    Initial commit

--- src/LINKEDLIST/Implementation.java
+++ src/LINKEDLIST/Implementation.java
@@ -1,7 +1,5 @@
 package LINKEDLIST;
 
-import java.util.Collections;
-import java.util.LinkedList;
 
 public class Implementation
 {
@@ -86,6 +84,53 @@ public class Implementation
             }
             size++;
         }
+        int getAt(int pos)
+        {
+            if(pos<0 || pos>=size)
+            {
+                System.out.println("Wrong position");
+                return -1;
+            }
+            Node temp=head;
+            for(int i=0;i<pos;i++)
+            {
+                temp=temp.next;
+            }
+            return temp.data;
+        }
+        void deleteAt(int pos)
+        {
+            if(pos<0 || pos>=size)
+            {
+                System.out.println("Deletion not possible at this position");
+                return ;
+            }
+            Node temp=head;
+            if(pos==0)
+            {
+                head=head.next;
+                size--;
+                return;
+            }
+            if(pos==size-1)
+            {
+                for(int i=1;i<pos;i++)
+                {
+                    temp=temp.next;
+                }
+                temp.next=null;
+                tail=temp;
+            }
+            else
+            {
+                for(int i=1;i<pos;i++)
+                {
+                    temp=temp.next;
+                }
+                temp.next=temp.next.next;
+            }
+            size--;
+        }
 
         public  void display()
         {
@@ -98,7 +143,7 @@ public class Implementation
             System.out.println();
         }
     }
-    public static void main(String[] args) throws NullPointerException
+    public static void main(String[] args)
     {
         linkedList ll=new linkedList();
         ll.insertAtEnd(10);
@@ -110,19 +155,31 @@ public class Implementation
         ll.insertAtBeg(70);
 
         ll.display();
-        System.out.println(ll.tail.data);
-        System.out.println(ll.head.data);
+        System.out.println("head="+ll.head.data);
+        System.out.println("tail="+ll.tail.data);
         System.out.println("size="+ll.size);
 
         System.out.println();
-        ll.insertAtPos(8,2  );
+        ll.insertAtPos(2,1000  );
 
 
         ll.display();
 
-        System.out.println(ll.tail.data);
-        System.out.println(ll.head.data);
 
+        System.out.println("head="+ll.head.data);
+        System.out.println("tail="+ll.tail.data);
+
+        System.out.println("size="+ll.size);
+        System.out.println("data at 5th position="+ll.getAt(5));
+        System.out.println();
+
+        ll.deleteAt(6);
+        ll.display();
+
+        ll.deleteAt(7);
+
+        System.out.println(ll.head.data);
+        System.out.println(ll.tail.data);
         System.out.println("size="+ll.size);
 
     }
